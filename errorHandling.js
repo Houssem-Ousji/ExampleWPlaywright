88843f65be362bced48caa4b9a0a326439067707
Houssem <86334640+Houssem-Ousji@users.noreply.github.com>
2023-02-07 11:04:07 +0100

    Update errorHandling.js

--- errorHandling.js
+++ errorHandling.js
@@ -1,5 +1,5 @@
-module.exports.errorHandler = async (error,page,browser) =>{
-    await page.screenshot({ path: 'screenshots/step_failed.png', fullPage: true });
+module.exports.errorHandler = async (error,page,browser,step) =>{
+    await page.screenshot({ path: `screenshots/step_failed${step}.png`, fullPage: true });
     await browser.close()
     throw new Error(error.toString())
 }
