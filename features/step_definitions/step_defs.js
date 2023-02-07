af51bdaf1e7bd4d7e223c72d8ee6da5eeda7ec1a
Houssem <86334640+Houssem-Ousji@users.noreply.github.com>
2023-02-07 11:05:26 +0100

    Update step_defs.js

--- features/step_definitions/step_defs.js
+++ features/step_definitions/step_defs.js
@@ -2,7 +2,7 @@ const assert = require("assert");
 const { Given, When, Then } = require("@cucumber/cucumber");
 const { Browser, chromium, Page } = require("playwright");
 const { expect } = require("@playwright/test");
-const { errorHandler} = require("../../errorHandling");
+const { errorHandler } = require("../../errorHandling");
 
 
 let browser = Browser;
@@ -14,33 +14,41 @@ async function navigate() {
   await page.goto("https://www.demoblaze.com/");
   console.log(`captured site title as ${await page.title()}`);
 }
-async function close(){
+async function close() {
   await browser.close()
 }
 Given("I am on {string} page", async function (expectedTitle) {
- try {
-  await navigate();
-  const title = await page.locator("#nava").textContent();
-  expect(expectedTitle).toEqual(title.trim());
-  await page.screenshot({ path: 'screenshots/first_step.png', fullPage: true });
- } catch (error) {
-   await errorHandler(error,page,browser)
- }
+  try {
+    await navigate();
+    const title = await page.locator("#nava").textContent();
+    expect(expectedTitle).toEqual(title.trim());
+    await page.screenshot({ path: 'screenshots/first_step.png', fullPage: true });
+  } catch (error) {
+    await errorHandler(error, page, browser,"1")
+  }
 });
 When("I click categories link", async function () {
-  const categories_button = await page.locator("#cat").click();
-  await page.screenshot({ path: 'screenshots/second_step.png', fullPage: true });
+  try {
+    const categories_button = await page.locator("#cat").click();
+    await page.screenshot({ path: 'screenshots/second_step.png', fullPage: true });
+  } catch (error) {
+    await errorHandler(error, page, browser,"2")
+  }
 });
 Then(
   "I see {string} , {string} and {string} under categories",
-  async function (string,string1,string2) {
-    const phones = await page.getByRole('link',{ name: 'Phones' }).textContent();
-    const laptops = await page.getByRole('link',{ name: 'Laptops' }).textContent();
-    const monitoring = await page.getByRole('link',{ name: 'Monitors' }).textContent();
-    expect(string).toEqual(phones.trim());
-    expect(string1).toEqual(laptops.trim());
-    expect(string2).toEqual(monitoring.trim());
-    await page.screenshot({ path: 'screenshots/third_step.png', fullPage: true });
-    await close();
+  async function (string, string1, string2) {
+    try {
+      const phones = await page.getByRole('link', { name: 'Phones' }).textContent();
+      const laptops = await page.getByRole('link', { name: 'Laptops' }).textContent();
+      const monitoring = await page.getByRole('link', { name: 'Monitors' }).textContent();
+      expect(string).toEqual(phones.trim());
+      expect(string1).toEqual(laptops.trim());
+      expect(string2).toEqual(monitoring.trim());
+      await page.screenshot({ path: 'screenshots/third_step.png', fullPage: true });
+      await close();
+    } catch (error) {
+      await errorHandler(error, page, browser,"3")
+    }
   }
 );
