2e4c5c1ee24a7c664ca2530502635c36830f8990
Houssem <86334640+Houssem-Ousji@users.noreply.github.com>
2023-02-07 09:55:35 +0100

    Update step_defs.js

--- features/step_definitions/step_defs.js
+++ features/step_definitions/step_defs.js
@@ -2,6 +2,8 @@ const assert = require("assert");
 const { Given, When, Then } = require("@cucumber/cucumber");
 const { Browser, chromium, Page } = require("playwright");
 const { expect } = require("@playwright/test");
+const { errorHandler} = require("../../errorHandling");
+
 
 let browser = Browser;
 let page = Page;
@@ -12,14 +14,22 @@ async function navigate() {
   await page.goto("https://www.demoblaze.com/");
   console.log(`captured site title as ${await page.title()}`);
 }
-
+async function close(){
+  await browser.close()
+}
 Given("I am on {string} page", async function (expectedTitle) {
+ try {
   await navigate();
   const title = await page.locator("#nava").textContent();
   expect(expectedTitle).toEqual(title.trim());
+  await page.screenshot({ path: 'screenshots/first_step.png', fullPage: true });
+ } catch (error) {
+   await errorHandler(error,page,browser)
+ }
 });
 When("I click categories link", async function () {
   const categories_button = await page.locator("#cat").click();
+  await page.screenshot({ path: 'screenshots/second_step.png', fullPage: true });
 });
 Then(
   "I see {string} , {string} and {string} under categories",
@@ -30,5 +40,7 @@ Then(
     expect(string).toEqual(phones.trim());
     expect(string1).toEqual(laptops.trim());
     expect(string2).toEqual(monitoring.trim());
+    await page.screenshot({ path: 'screenshots/third_step.png', fullPage: true });
+    await close();
   }
 );
